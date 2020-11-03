88383653ba986e7f1a9b5c135c79ef9481a7f462
Fernando Vaz <contact@fernandomvaz.com>
2020-11-03 19:45:47 -0300

    add of cors dependency and other setting

--- app.js
+++ app.js
@@ -3,8 +3,20 @@ var csv = require("csvtojson");
 
 var indexRouter = require('./routes/index');
 var dataRouter = require('./routes/data');
+var cors = require('cors')
 //var listenRouter = require('./routes/port')
-var app = express();
+var app = express()
+app.use(cors())
+var corsOptions1 = {
+    origin: 'http://localhost:4200',
+    optionsSuccessStatus: 200
+}
+var corsOptions2 = {
+    origin: 'http://localhost:3000/data',
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions2))
+app.use(cors(corsOptions1))
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
